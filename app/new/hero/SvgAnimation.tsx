60677a7093e18b1299ef7456f6dad11ff37c642b
sina <sinaa.afshar@gmail.com>
2025-08-20 18:40:37 +0330

    edit svg font

--- app/new/hero/SvgAnimation.tsx
+++ app/new/hero/SvgAnimation.tsx
@@ -21,25 +21,25 @@ export default function SvgAnimation() {
 
   return (
     <svg width="600px" height="500px" direction="ltr" className="absolute top-12 left-12">
-      <text id="text1" x="40" y="200" fontFamily="Tahoma" fontSize="24" fill="white">
+      <text id="text1" x="40" y="200" fontSize="24" fill="white" className="font-dana font-bold">
         حسابداری
       </text>
       <rect id="rect1" x="30" y="160" width="115" height="60" fill="#7a663f25" />
 
       <line id="line1" x1="150" y1="195" x2="250" y2="100" stroke="#8a8a8a" strokeWidth="3" />
-      <text id="text2" x="250" y="100" fontFamily="Tahoma" fontSize="24" fill="white">
+      <text id="text2" x="250" y="100" fontSize="24" fill="white" className="font-dana font-bold">
         سیستماتیک
       </text>
       <rect id="rect2" x="250" y="60" width="120" height="60" fill="#7a663f25" />
 
       <line id="line2" x1="150" y1="195" x2="250" y2="200" stroke="#8a8a8a" strokeWidth="3" />
-      <text id="text3" x="290" y="200" fontFamily="Tahoma" fontSize="24" fill="white">
+      <text id="text3" x="290" y="200" fontSize="24" fill="white" className="font-dana font-bold">
         امن
       </text>
       <rect id="rect3" x="250" y="160" width="120" height="60" fill="#7a663f25" />
 
       <line id="line3" x1="150" y1="195" x2="250" y2="300" stroke="#8a8a8a" strokeWidth="3" />
-      <text id="text4" x="270" y="300" fontFamily="Tahoma" fontSize="24" fill="white">
+      <text id="text4" x="270" y="300" fontSize="24" fill="white" className="font-dana font-bold">
         تطبیقی
       </text>
       <rect id="rect4" x="250" y="260" width="120" height="60" fill="#7a663f25" />
